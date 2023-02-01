6fdadd284f4909dd91ce28b048e1e209e79f696b
osezei <john.osezei@yahoo.com>
2023-02-01 11:13:27 +0100

    second batch

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -63,7 +63,7 @@ const SingleProductPage = () => {
           <ProductImages images={images} />
           <section className="content">
             <h2>{name}</h2>
-            <Stars />
+            <Stars stars={stars} reviews={reviews} />
             <h5 className="price">{formatPrice(price)}</h5>
             <p className="desc">{description}</p>
             <p className="info">
@@ -78,6 +78,8 @@ const SingleProductPage = () => {
               <span>Brand : </span>
               {company}
             </p>
+            <hr />
+            {stock > 0 && <AddToCart product={product} />}
           </section>
         </div>
       </div>
